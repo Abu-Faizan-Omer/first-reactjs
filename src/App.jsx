ea179e6c94c81b1f28a868c9c36af6b516f1ed5e
Abu-Faizan-Omer <abufaizan1997@gmail.com>
2025-07-12 19:34:55 +0530

    Creating Reusable Components

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,31 @@ import ExpenseItem from "./components/Expenses/ExpenseItem";
 
 
 function App() {
+  const expenses = [
+    { id: "1", location : "Bangalore", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
+    { id: "2", location : "Delhi",date: new Date(2023, 3, 25), title: "Book", price: 10 },
+    { id: "3", location : "Hyderabad", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+    { id: "4", location : "Mumbai", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
+  ];
+
   return (
     <div>
       <h1>Let's get Started</h1>
 
-      <ExpenseItem
-        date={new Date(2023, 7, 15)}
-        title="Insurance"
-        price="50"
-        location="Bangalore"
-      ></ExpenseItem>
-
-      <ExpenseItem
-        date={new Date(2023, 3, 25)}
-        title="Book"
-        price="20"
-        location="Delhi"
-      ></ExpenseItem>
-
-      <ExpenseItem
-        date={new Date(2023, 10, 11)}
-        title="Pen"
-        price="5"
-        location="Hyderabad"
-      ></ExpenseItem>
-
-      <ExpenseItem
-        date={new Date(2023, 1, 14)}
-        title="Laptop"
-        price="200"
-        location="Mumbai"
-      ></ExpenseItem>
+      {expenses.map((expense, index) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            location={expense.location}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+          ></ExpenseItem>
+        );
+      })}
       
     </div>
   );
 }
+
 export default App
